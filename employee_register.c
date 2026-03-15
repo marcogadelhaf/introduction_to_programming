190482c5b08932000a367e49b8e564acab158895
Marco <86749822+marcogadelhaf@users.noreply.github.com>
2026-03-15 17:35:13 -0300

    Refactor employee struct and related functions

--- employee_register.c
+++ employee_register.c
@@ -1,66 +1,74 @@
 #include <stdio.h>
 #include <string.h>
 
-#define MAX 50
+typedef struct
+{
+  char nome[50];
+  int idade;
+  char cargo[50];
+  float salario;
+}tFuncionario;
 
-// Struct Funcionario
-typedef struct {
-    char nome[50];
-    int idade;
-    char cargo[30];
-    float salario;
-} Funcionario;
-
-// Employee registration
-void cadastrar(Funcionario func[], int *n) {
-    printf("Nome: ");
-    scanf(" %[^\n]", func[*n].nome);
-    printf("Idade: ");
-    scanf("%d", &func[*n].idade);
-    printf("Cargo: ");
-    scanf(" %[^\n]", func[*n].cargo);
-    printf("Salario: ");
-    scanf("%f", &func[*n].salario);
-    (*n)++;
+void cadastrar(tFuncionario funcionario[], int *n)
+{
+  printf("Nome: ");
+  scanf(" %[^\n]", funcionario[*n].nome);
+  printf("Idade: ");
+  scanf(" %d", &funcionario[*n].idade);
+  printf("Cargo: ");
+  scanf(" %[^\n]", funcionario[*n].cargo);
+  printf("Salário: ");
+  scanf("%f", &funcionario[*n].salario);
+(*n)++;  
 }
 
-// Employee search
-void buscar(Funcionario func[], int n) {
-    char nomeBusca[50];
-    printf("Nome a buscar: ");
-    scanf(" %[^\n]", nomeBusca);
-    
-    for(int i = 0; i < n; i++) {
-        if(strcmp(func[i].nome, nomeBusca) == 0) {
-            printf("\nNome: %s\n", func[i].nome);
-            printf("Idade: %d\n", func[i].idade);
-            printf("Cargo: %s\n", func[i].cargo);
-            printf("Salario: %.2f\n", func[i].salario);
-            return;
-        }
+void buscar(tFuncionario funcionario[], int n)
+{
+  char buscaNome[50];
+  
+  printf("Nome para a busca: \n");
+  scanf(" %[^\n]", buscaNome);
+  
+  for(int i = 0 ; i < n ; i++)
+  {
+    if(strcmp(funcionario[i].nome, buscaNome) == 0)
+    {
+      printf("Nome: %s\n", funcionario[i].nome);
+      printf("Idade: %d\n", funcionario[i].idade);
+      printf("Cargo: %s\n", funcionario[i].cargo);
+      printf("Salário: %f\n", funcionario[i].salario);
+      return;
     }
-    printf("Funcionario nao encontrado!\n");
+    else
+    {
+      printf("Esse funcionário não foi encontrado.\n");
+    }
+  }
 }
+int main()
+{
+  tFuncionario funcionarios[50];
+  int n = 0;
+  int op;
+  
+  do
+  {
+    printf("====== SELECIONE A OPÇÃO DESEJADA ======\n1: CADASTRO\t\t2: BUSCA\n0: SAIR\n");
+    scanf("%d", &op);
+    
+    switch(op)
+    {
+      case 1:
+      cadastrar(funcionarios, &n);
+      break;
+      case 2:
+      buscar(funcionarios, n);
+      break;
+    }
+  }while(op != 0);
 
-int main() {
-    Funcionario funcionarios[MAX];
-    int n = 0, op;
-    
-    do {
-        printf("\n1-Cadastrar\n2-Buscar\n0-Sair\nOpcao: ");
-        scanf("%d", &op);
-        
-        switch(op) {
-            case 1: cadastrar(funcionarios, &n);
-            break;
-            case 2: buscar(funcionarios, n);
-            break;
-        }
-    } while(op != 0);
-    
-    return 0;
+return 0;
 }
-
 // if struct array: Funcionario funcionarios[100];
 // if read name with spacebar (\n): scanf(" %[^\n]", funcionarios[i].nome);
 // if search of a name: if(strcmp(funcionarios[i].nome, nomeBusca) == 0) {}
